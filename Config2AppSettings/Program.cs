826ba6242e97ce2658b1713b4edc08f552996924
rameshkartik <rameshkartik@gmail.com>
2025-04-28 12:15:00 +0530

    Add files via upload

---
+++ Config2AppSettings/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Config2AppSettings;
+string path = @"C:\\SourceCode\\poc_net8.0_m2madapter\\LandisGyr.CellularNMS.NetworkMesssageProcessor.HostCore\Web.config";
+string appSettingsJsonpath = "C:\\SourceCode\\Config2AppSettings\\appSettings.json";
+Console.WriteLine("Hello, World!");
+
+ConverterLibrary converterLibrary = new ConverterLibrary(path, appSettingsJsonpath);
+
+converterLibrary.Convert2AppSettings();
+
+Console.WriteLine();
+
